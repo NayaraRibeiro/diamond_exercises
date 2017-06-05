3d8d8f4391eb75fdcbc56c2e156b6a399416296a
NayaraRibeiro <nayararibeiro@gmail.com>
2017-06-05 11:01:21 -0300

    Draw a centered diamond

--- src/main/java/exercises/Diamond.java
+++ src/main/java/exercises/Diamond.java
@@ -22,6 +22,31 @@ public class Diamond {
     }
 
     public String centeredDiamond(Integer numberOfLines) {
-        return  null;
+        String topDiamond = "";
+        String bottomDiamond = "";
+
+        for (int index = 0; index < numberOfLines; index++) {
+            for (int kIndex = 0; kIndex < numberOfLines - index; kIndex++) {
+                topDiamond += SPACE;
+            }
+            for (int jIndex = 0; jIndex < index * 2 + 1; jIndex++) {
+                topDiamond += ASTERISK;
+            }
+            topDiamond += NEWLINE;
+        }
+
+        for (int row = numberOfLines - 1; row > 0; row--)
+        {
+            for (int kIndex = row; kIndex < numberOfLines + 1; kIndex++) {
+                bottomDiamond += SPACE;
+            }
+
+            for (int numberOfAsterisks = (row * 2) - 1; numberOfAsterisks > 0; numberOfAsterisks--)
+            {
+                bottomDiamond += ASTERISK;
+            }
+            bottomDiamond += NEWLINE;
+        }
+        return topDiamond + bottomDiamond;
     }
 }
